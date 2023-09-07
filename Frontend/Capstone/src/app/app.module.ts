458a7c8093612106cbc42f3015e9400434487f35
GeraldFishta <ge.fishta@gmail.com>
2023-09-07 16:35:24 +0200

    avanzo

--- Frontend/Capstone/src/app/app.module.ts
+++ Frontend/Capstone/src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './components/form/form.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormComponent,
+    HomepageComponent,
+    ReservationComponent,
+    HomeComponent,
+    AboutComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/Capstone/src/app/pages/reservation/reservation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './reservation.component.html',
+  styleUrls: ['./reservation.component.scss']
+})
+export class ReservationComponent {
+
+}
